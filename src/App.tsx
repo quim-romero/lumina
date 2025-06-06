78a7bf318a1528a794c2f0ed5889f71aebc419a1
quim-romero <quimromero22@gmail.com>
2025-06-06 11:45:00 +0200

    feat: complete App routing with loader, scroll, layout, and all page routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,18 @@ import ProductGrid from "./components/ProductGrid";
 import ContactForm from "./components/ContactForm";
 import Loader from "./components/Loader";
 import ProductPage from "./components/ProductPage";
-import CartPage from "./pages/CartPage";
 
+import CartPage from "./pages/CartPage";
 import SuccessPage from "./pages/SuccessPage";
 import CancelPage from "./pages/CancelPage";
+
+import { useScrollTop } from "./hooks/useScrollTop";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
   const location = useLocation();
+
+  useScrollTop();
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -27,51 +31,53 @@ export default function App() {
   if (isLoading) return <Loader />;
 
   return (
-    <AnimatePresence mode="wait" initial={false}>
-      <Routes location={location} key={location.pathname}>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Hero />
-              <ProductGrid />
-              <ContactForm />
-            </Layout>
-          }
-        />
-        <Route
-          path="/product/:slug"
-          element={
-            <Layout>
-              <ProductPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/cart"
-          element={
-            <Layout>
-              <CartPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/success"
-          element={
-            <Layout>
-              <SuccessPage />
-            </Layout>
-          }
-        />
-        <Route
-          path="/cancel"
-          element={
-            <Layout>
-              <CancelPage />
-            </Layout>
-          }
-        />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Hero />
+                <ProductGrid />
+                <ContactForm />
+              </Layout>
+            }
+          />
+          <Route
+            path="/product/:slug"
+            element={
+              <Layout>
+                <ProductPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/success"
+            element={
+              <Layout>
+                <SuccessPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/cancel"
+            element={
+              <Layout>
+                <CancelPage />
+              </Layout>
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Layout>
+                <CartPage />
+              </Layout>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
