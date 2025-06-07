824c0b2aadf4b90c6808f0bc52111b77467562fb
quim-romero <quimromero22@gmail.com>
2025-06-07 13:05:00 +0200

    feat: add useScrollTop hook to Layout for scroll reset on route change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,15 @@
 import type { ReactNode } from "react";
-import Navbar from "./Navbar";
+import { useScrollTop } from "../hooks/useScrollTop";
 import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 type Props = {
   children: ReactNode;
 };
 
 export default function Layout({ children }: Props) {
+  useScrollTop();
+
   return (
     <>
       <Navbar />
